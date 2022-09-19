318ce8fd8572520368bd691f8a8fcaab35b6a0aa
JerryMartin3407 <113940123+JerryMartin3407@users.noreply.github.com>
2022-09-19 14:46:22 -0500

    Add files via upload

--- src/main/java/com/jmartin/demo/config/Config.java
+++ src/main/java/com/jmartin/demo/config/Config.java
@@ -1,1 +1,21 @@
+package com.jmartin.demo.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+@Configuration
+public class Config {
+	@Bean
+	public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+		JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+
+		return jdbcTemplate;
+	}
+
+}

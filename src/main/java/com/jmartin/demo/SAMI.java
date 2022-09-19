96c3f1432956f12f24802ad2703d27a4ca62618f
JerryMartin3407 <113940123+JerryMartin3407@users.noreply.github.com>
2022-09-19 14:42:11 -0500

    Add files via upload

--- src/main/java/com/jmartin/demo/SAMI.java
+++ src/main/java/com/jmartin/demo/SAMI.java
@@ -1,1 +1,38 @@
+package com.jmartin.demo;
 
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+
+
+
+
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+@SpringBootApplication
+@EnableScheduling
+@RestController
+@EnableAutoConfiguration
+
+public class SAMI implements CommandLineRunner {
+	public static void main(String[] args) {
+		SpringApplication.run(SAMI.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("SAMI - Search Actor Movie Index");
+	}
+
+    @GetMapping("/error")
+	String noData(){
+		return "SAMI Error - No Data Found";
+	}
+}
